888aaf3bfad94058af99183e3f68932c7179273f
Hiếu <54101547+xuanhieu147@users.noreply.github.com>
2021-01-27 10:52:20 +0700

    complete login function

--- app/src/main/java/com/example/chatwithfirebase/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/chatwithfirebase/ui/login/LoginViewModel.kt
@@ -1,11 +1,59 @@
 package com.example.chatwithfirebase.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import com.example.chatwithfirebase.base.BaseViewModel
+import com.example.chatwithfirebase.utils.EmailUtil
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor():BaseViewModel() {
 
-    fun login(){
-        firebaseRepository
+    companion object {
+        // error
+        const val ERROR_EMAIL_FORMAT = 10
+        const val ERROR_TYPE_EMAIL = 9
+        const val ERROR_TYPE_PASSWORD = 8
+
+        // success
+        const val LOGIN_SUCCESS = 7
+    }
+
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    fun loginUser() {
+        setLoading(true)
+        when {
+            email.value.isNullOrEmpty() -> {
+                setLoading(false)
+                uiEventLiveData.value = ERROR_TYPE_EMAIL
+            }
+
+            !EmailUtil.checkValidEmail(email.value) -> {
+                setLoading(false)
+                uiEventLiveData.value = ERROR_EMAIL_FORMAT
+            }
+
+            password.value.isNullOrEmpty() -> {
+                setLoading(false)
+                uiEventLiveData.value = ERROR_TYPE_PASSWORD
+            }
+
+            else -> {
+                compositeDisposable.add(
+                    firebaseRepository.login(email.value!!,password.value!!)
+                        .compose(schedulerProvider.ioToMainCompletableScheduler())
+                        .subscribe(this::onLoginSuccessful, this::onLoginFailed))
+            }
+        }
+    }
+
+    private fun onLoginSuccessful(){
+        setLoading(false)
+        uiEventLiveData.value = LOGIN_SUCCESS
+    }
+
+    private fun onLoginFailed(t:Throwable){
+        setLoading(false)
+        showError(t)
     }
 }
